df03d8dcdbb060fe4da66f22649fa7b5b229da67
ally1031 <yutaro86win@gmail.com>
2020-10-05 21:17:54 +0900

    出品画像のバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
-  validates :title, :text, :category_id, :status_id, :shipping_charges_id, :shipment_source_id, :shipping_days_id, :price, precence: true
+  has_one_attached :image
+  #validates :image, :title, :text, :category_id, :status_id, :shipping_charges_id, :shipment_source_id, :shipping_days_id, :price, precence: true
   
 end
