0e659e7fcccd8f787bbb672af8598ea59cd3e275
ally1031 <yutaro86win@gmail.com>
2020-10-05 16:45:26 +0900

    fakerの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.name}
-    email                 {Faker::Internet.email(separators: '@')}
-    password              {Faker::Internet.password(min_length: 6)}
+    email                 {Faker::Internet.email}
+    password              {"aaa000"}
     password_confirmation {password}
-    family_name           {Faker::Name.name}
-    first_name            {Faker::Name.first_name}
-    family_name_kana      {Faker::Name.name}
-    first_name_kana       {Faker::Name.first_name}
+    family_name           {"田中"}
+    first_name            {"たろう"}
+    family_name_kana      {"タナカ"}
+    first_name_kana       {"タロウ"}
     birthday              {Faker::Date.birthday}
   end
 end
