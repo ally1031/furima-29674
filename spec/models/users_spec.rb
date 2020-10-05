72c40fb00e13ae40dbe654c9722f8be418a36012
ally1031 <yutaro86win@gmail.com>
2020-10-05 16:46:12 +0900

    テストコードの追加

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -12,19 +12,6 @@ RSpec.describe User, type: :model do
       it "nicknameとemail、passwordとpassword_confirmation、family_nameとfirst_name、family_name_kanaとfirst_name_kana、birthdayが存在すれば登録できる" do
         expect(@user).to be_valid
       end
-
-      it "passwordが半角英数字混同であれば登録できる" do
-        @user.password = "000000"
-        @user.password_confirmation = "000000"
-        expect(@user).to be_valid
-      end
-
-      it "family_nameとfirst_nameが全角（漢字・ひらがな・カタカナ）であれば登録できる" do
-      end
-
-      it "family_name_kanaとfirst_name_kanaが全角（カタカナ）であれば登録できる" do
-      end
-
     end
 
     context '新規登録がうまくいかないとき' do
@@ -41,13 +28,6 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
       
-      it "重複したemailが存在する場合登録できないこと" do
-        @user.save
-        another_user = FactoryBot.build(:user, email: @user.email)
-        another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
-      end
-
       it "emailに＠がないと登録できない" do
         @user.email = "dsdasgmail.com"
         @user.valid?
@@ -69,7 +49,7 @@ RSpec.describe User, type: :model do
       it "passwordが6文字以下では登録できない" do
         @user.password = "00000"
         @user.valid?
-        expect(@user.errors.full_messages).to include()
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is too short (minimum is 6 characters)")
       end
 
       it "苗字が空だと登録できない" do
@@ -84,6 +64,18 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       
+      it "family_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない" do
+        @user.family_name = "csxs"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name is invalid")
+      end
+
+      it "first_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない" do
+        @user.first_name = "sjbd"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+
       it "苗字（カナ）が空だと登録できない" do
         @user.family_name_kana = ""
         @user.valid?
@@ -96,7 +88,19 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
       
-      it "誕生日が空だと登録できない" do
+      it "family_name_kanaが全角（カタカナ）でないと登録できない" do
+        @user.family_name_kana = "dvsf"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana is invalid")
+      end
+
+      it "first_name_kanaが全角（カタカナ）でないと登録できない" do
+        @user.first_name_kana = "dcsg"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+
+        it "誕生日が空だと登録できない" do
         @user.birthday = ""
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
