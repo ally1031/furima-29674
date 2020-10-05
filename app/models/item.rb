26f47b75ffc0d8ad95d2fcbb6c24226ed02073b2
ally1031 <yutaro86win@gmail.com>
2020-10-05 20:43:11 +0900

    バリデーションの追加
    全ての出品情報が必須であること

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  
+  validates :title, :text, :category_id, :status_id, :shipping_charges_id, :shipment_source_id, :shipping_days_id, :price, precence: true
+  
 end
