036311894cdf2fdd70d74f7ba28879da3514b482
ally1031 <yutaro86win@gmail.com>
2020-10-05 16:44:02 +0900

    donation_paramsの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,11 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @item = Item.all
+    #@item = Item.all
+  end
+
+  private
+   def donation_params
+    params.require(:user).permit(:email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana)
   end
 end
