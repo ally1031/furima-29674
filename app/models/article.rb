383390fd19dc469cc938a7a681ede88a721c5944
ally1031 <yutaro86win@gmail.com>
2020-10-06 20:32:20 +0900

    articleの削除
    本実装とは関係ない為

--- app/models/article.rb
+++
@@ -1,7 +0,0 @@
-class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre, :status_ah, :shipping_charges_ah, :shipment_source_ah, :shipping_days_ah
-
-  validates :title, :text, :price, presence: true
-  validates :genre_id, :status_ah_id, :shipping_charges_ah_id, :shipment_source_ah_id, :shipping_days_ah_id numericality: { other_than: 1 } 
-end
