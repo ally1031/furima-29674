26902d0e573aedeffbe34f15016eb2d0762e1d32
ally1031 <yutaro86win@gmail.com>
2020-10-06 20:36:03 +0900

    itemsのバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  #validates :image, :title, :text, :category_id, :status_id, :shipping_charges_id, :shipment_source_id, :shipping_days_id, :price, precence: true
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :status_ah
+  belongs_to_active_hash :shipping_charges_ah
+  belongs_to_active_hash :shipment_source_ah
+  belongs_to_active_hash :shipping_days_ah
+
+  validates :image, :title, :text, :genre_id, :status_ah_id, :shipping_charges_ah_id, :shipment_source_ah_id, :shipping_days_ah_id, :price, presence: true
+  validates :genre_id, :status_ah_id, :shipping_charges_ah_id, :shipment_source_ah_id, :shipping_days_ah_id, numericality: { other_than: 1 }
 end
