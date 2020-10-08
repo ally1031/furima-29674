f193bbf78e5927f70910200bf4ebb01f78e54f33
ally1031 <yutaro86win@gmail.com>
2020-10-08 18:33:28 +0900

    FactoryBotの追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,17 @@
 FactoryBot.define do
-  factory :items do
+  factory :item do
+   title                       {'aaa'}
+   text                        {'aaa'}
+   genre_id                    {2}
+   status_ah_id                {2}
+   shipping_charges_ah_id      {3}
+   shipment_source_ah_id       {4}
+   shipping_days_ah_id         {5}
+   price                       {500}
+   association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/camera.png'), filename: 'camera.png')
+    end
   end
 end
