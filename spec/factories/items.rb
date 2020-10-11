affcbbbe6ded421bd0427767003e69c48ab93f15
ally1031 <yutaro86win@gmail.com>
2020-10-11 21:53:02 +0900

    rubocop

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :item do
-   title                       {'aaa'}
-   text                        {'aaa'}
-   genre_id                    {2}
-   status_ah_id                {2}
-   shipping_charges_ah_id      {3}
-   shipment_source_ah_id       {4}
-   shipping_days_ah_id         {5}
-   price                       {500}
-   association :user
+    title { 'aaa' }
+    text                        { 'aaa' }
+    genre_id                    { 2 }
+    status_ah_id                { 2 }
+    shipping_charges_ah_id      { 3 }
+    shipment_source_ah_id       { 4 }
+    shipping_days_ah_id         { 5 }
+    price                       { 500 }
+    association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/image/Rails.test.png'), filename: 'Rails.test.png')
--- spec/models/items_spec.rb
+++ spec/models/items_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe '商品出品機能' do
-    
     before do
       @item = FactoryBot.build(:item)
     end
@@ -13,13 +12,12 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品出品機能がうまくいかないとき' do
-      
       it 'imageが空だと登録できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      
+
       it 'titleが空では登録できない' do
         @item.title = ''
         @item.valid?
@@ -32,64 +30,64 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Text can't be blank")
       end
 
-     it 'genre_idが空では登録できない' do
+      it 'genre_idが空では登録できない' do
         @item.genre_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Genre can't be blank", "Genre is not a number")
+        expect(@item.errors.full_messages).to include("Genre can't be blank", 'Genre is not a number')
       end
 
       it 'genre_idで1を選択した場合では登録できない' do
         @item.genre_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Genre must be other than 1")
+        expect(@item.errors.full_messages).to include('Genre must be other than 1')
       end
 
       it 'status_ah_idだと登録できない' do
         @item.status_ah_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status ah can't be blank", "Status ah is not a number")
+        expect(@item.errors.full_messages).to include("Status ah can't be blank", 'Status ah is not a number')
       end
 
       it 'status_ah_idで1を選択した場合だと登録できない' do
         @item.status_ah_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status ah must be other than 1")
+        expect(@item.errors.full_messages).to include('Status ah must be other than 1')
       end
 
       it 'shipping_charges_ah_idが空では登録できない' do
         @item.shipping_charges_ah_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping charges ah can't be blank", "Shipping charges ah is not a number")
+        expect(@item.errors.full_messages).to include("Shipping charges ah can't be blank", 'Shipping charges ah is not a number')
       end
 
       it 'shipping_charges_ah_idで1を選択した場合では登録できない' do
         @item.shipping_charges_ah_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping charges ah must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping charges ah must be other than 1')
       end
 
       it 'shipment_source_ah_idが空では登録できない' do
         @item.shipment_source_ah_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipment source ah can't be blank", "Shipment source ah is not a number")
+        expect(@item.errors.full_messages).to include("Shipment source ah can't be blank", 'Shipment source ah is not a number')
       end
 
       it 'shipment_source_ah_idで1を選択した場合では登録できない' do
         @item.shipment_source_ah_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipment source ah must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipment source ah must be other than 1')
       end
 
       it 'shipping_days_ah_idが空だと登録できない' do
         @item.shipping_days_ah_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping days ah can't be blank", "Shipping days ah is not a number")
+        expect(@item.errors.full_messages).to include("Shipping days ah can't be blank", 'Shipping days ah is not a number')
       end
 
       it 'shipping_days_ah_idで1を選択した場合だと登録できない' do
         @item.shipping_days_ah_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping days ah must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping days ah must be other than 1')
       end
 
       it 'priceが空だと登録できない' do
@@ -101,20 +99,20 @@ RSpec.describe Item, type: :model do
       it '価格の範囲が、¥299より小さいと登録できない' do
         @item.price = 222
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+        expect(@item.errors.full_messages).to include('Price must be greater than 299')
       end
-      
+
       it '価格の範囲が、¥10000000より大きいと登録できない' do
-        @item.price = 100000000
+        @item.price = 100_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+        expect(@item.errors.full_messages).to include('Price must be less than 10000000')
       end
 
       it '価格は半角数字のみでないと登録できない' do
-        @item.price = "５５５"
+        @item.price = '５５５'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
-  end 
+  end
 end
