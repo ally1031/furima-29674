ff91351de48d9a6c650fc81876abbeccf4f6a8f4
ally1031 <yutaro86win@gmail.com>
2020-10-11 21:59:00 +0900

    updateメソッドの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,8 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if @item.update
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
