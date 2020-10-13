024cfe473d05a37459ec12e1d4363648f56362c8
ally1031 <yutaro86win@gmail.com>
2020-10-13 20:07:55 +0900

    order_addressの追加

---
+++ app/forms/order_address.rb
@@ -0,0 +1,15 @@
+class OrderAddress
+
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :token
+
+  validates :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, presence: true
+  validates :prefectures_id, numericality: { other_than: 1 }
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+  validates :phone_number, format: { with: /\A\d{11}\z/ }
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number ,order_id: order.id)
+  end
+end
