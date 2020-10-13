67be03f74f6687e10f3fa4868d735ba9396bee80
ally1031 <yutaro86win@gmail.com>
2020-10-13 21:00:22 +0900

    rubocop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,54 +1,45 @@
-class OrderController < ApplicationController
-
+class OrdersController < ApplicationController
   def index
-    @order = Order.new
+    @order = OrderAddress.new
     @item = Item.find(params[:item_id])
-    #@orders = @room.orders.includes(:user)
+    # @orders = @room.orders.includes(:user)
   end
 
   def create
-    @order = OrdersAddress.new(order_params)
+    @order = OrderAddress.new(order_params)
     @order.save
     if @order.valid?
       pay_item
       @order.save
-      return redirect_to root_path
+      redirect_to root_path
     else
-     render 'index'
-    end
-    @order = Order.new(order_params)
-    if @order.valid?
-      pay_item
-      @order.save
-      return redirect_to root_path
-    else
-     render 'index'
+      render 'index'
     end
   end
 
   private
 
   def order_params
-    params.require(:orders_address).permit(:postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number,:token).marge(user_id: current_user_id, item_id: params[:item.id],token: params[:token])
+    params.require(:orders_address).permit(:postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-   Payjp.api_key = "sk_test_de58444b5c040a231a5ab73f"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: order_params[:price],
-      card: order_params[:token],    # カードトークン
-      currency:'jpy'                 # 通貨の種類(日本円)
+      card: order_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類(日本円)
     )
   end
 end
 
-#def index
+# def index
 #  @message = Message.new
 #  @room = Room.find(params[:room_id])
 #  @messages = @room.messages.includes(:user)
-#end
+# end
 
-#def create
+# def create
 #  @room = Room.find(params[:room_id])
 #  @message = @room.messages.new(message_params)
 #  @message.save
@@ -58,10 +49,10 @@ end
 #    @messages = @room.messages.includes(:user)
 #    render :index
 #  end
-#end
+# end
 
-#private
+# private
 
-#def message_params
+# def message_params
 #  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-#end
+# end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = ()=> {
-  Payjp.setPublicKey("pk_test_XXXXXXXpk_test_c42ea3e6efda33219de41e9d");  // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);  // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
