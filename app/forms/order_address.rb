8feb13c90efbf7ab6551ed4341d220f8722f652f
ally1031 <yutaro86win@gmail.com>
2020-10-14 21:07:06 +0900

    バリデーションの変更

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,14 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :token
+  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :token, :user_id, :item_id,:order_id
 
-  validates :postal_code, :prefectures_id, :municipality, :address, :phone_number, presence: true
-  validates :prefectures_id, numericality: { other_than: 1 }
-  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :phone_number, format: { with: /\A\d{11}\z/ }
-
+  validates :municipality, :address, :token, presence: true
+  validates :prefectures_id, numericality: { other_than: 1 },presence: true
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ },presence: true
+  validates :phone_number, format: { with: /\A\d{11}\z/ },presence: true
+ 
   def save
-    order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    orders = Order.create!(item_id: item_id, user_id: user_id)
+    Address.create!(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: orders.id )
   end
 end
