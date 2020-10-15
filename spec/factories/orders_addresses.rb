97cf349112b08b02866a7ea78b2e8490c40254fa
ally1031 <yutaro86win@gmail.com>
2020-10-15 11:32:57 +0900

    factorybotの作成

--- spec/factories/orders_addresses.rb
+++ spec/factories/orders_addresses.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
-  factory :orders_address do
-    
+  factory :order_address do
+    token            { 'tok_abcdefghijk00000000000000000' }    
+    postal_code      { '123-1234' } 
+    prefectures_id   { 2 }   
+    municipality     { '札幌市' }    
+    address          { '1-1-1' }  
+    building_name    { 'ビル' }    
+    phone_number     { '09012345678' }   
+      
+    association :user    
+    association :item  
   end
 end
